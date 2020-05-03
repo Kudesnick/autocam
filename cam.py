3d91a048db9a4fe2d5318dbe6cda6352fe059daa
Kudesnick <kudesnick@inbox.ru>
2020-05-03 21:21:33 +0100

    [mod] wrap code to try/finally blocks

--- cam.py
+++ cam.py
@@ -10,10 +10,11 @@ cam_pre = 20
 cam_push = 21
 usb_pwr = 12
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup([cam_pwr, cam_pre, cam_push, usb_pwr], GPIO.IN)
+try:
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup([cam_pwr, cam_pre, cam_push, usb_pwr], GPIO.IN)
 
-if True:
     GPIO.setup(cam_pwr, GPIO.OUT, initial = GPIO.LOW)
     sleep(5)
     GPIO.setup(cam_pre, GPIO.OUT, initial = GPIO.LOW)
@@ -21,27 +22,31 @@ if True:
     GPIO.setup(cam_push, GPIO.OUT, initial = GPIO.LOW)
     sleep(1)
     GPIO.setup([cam_pre, cam_push], GPIO.IN)
-    sleep(5)
+    sleep(20)
     GPIO.setup(usb_pwr, GPIO.OUT, initial = GPIO.LOW)
-    sleep(15)
+    sleep(5)
 
-    mount('LABEL=CAM_SD', '/media')
-    sleep(1)
+    try:
+        mount('LABEL=CAM_SD', '/media')
+        sleep(1)
     
-    mnt_path = '/media/DCIM/100MSDCF'
-    cmd = ['python3', 'vk.py']
-    for name in os.listdir(mnt_path):
-        cmd.append(mnt_path + '/' + name)
+        mnt_path = '/media/DCIM/100MSDCF'
+        cmd = ['python3', 'vk.py']
+        for name in os.listdir(mnt_path):
+            cmd.append(mnt_path + '/' + name)
 
-    print('Send to server..')
-    subprocess.run(cmd)
+        print('Send to server..')
+        subprocess.run(cmd)
 
-    for name in os.listdir(mnt_path):
-        os.remove(mnt_path + '/' + name)
+        for name in os.listdir(mnt_path):
+            os.remove(mnt_path + '/' + name)
     
-    umount('/media')
+    finally:
+        umount('/media')
 
     GPIO.setup([cam_pwr, usb_pwr], GPIO.IN)
     sleep(3)
+finally:
+    GPIO.cleanup()
 
     print('Ok!')
