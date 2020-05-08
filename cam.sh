0c0e0e5795734cf57386f75ad9a462b6c8761d0b
Kudesnick <kudesnick@inbox.ru>
2020-05-08 10:51:21 +0100

    [fix] bash script

--- cam.sh
+++ cam.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd ~/autocam
+cd /home/pi/autocam
 
-if  [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]
-then
-    printf 'Repo update..'
+date
+
+if  [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]; then
+    printf 'Repo update..\r\n'
     git pull
-    printf 'Repo update complete.'
+    printf 'Repo update complete.\r\n'
 fi
 
-if  [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]
-then
-    printf 'Autocam send starting..'
+if  [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]; then
+    printf 'Autocam send starting..\r\n'
     sudo python3 cam.py send
-    printf 'Autocam finished.'
-elif [ $[$(date +"%M") % 10] == 0 ] && [ $[$(date +"%H") %  1] == 0 ]
-    printf 'Autocam starting..'
+    printf 'Autocam finished.\r\n'
+elif [ $[$(date +"%M") % 10] == 0 ] && [ $[$(date +"%H") %  1] == 0 ]; then
+    printf 'Autocam starting..\r\n'
     sudo python3 cam.py
-    printf 'Autocam finished.'
+    printf 'Autocam finished.\r\n'
 fi
