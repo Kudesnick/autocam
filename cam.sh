3164eaadc2eae1e23be6a368768dcbab24f37145
Kudesnick <kudesnick@inbox.ru>
2020-05-11 10:51:31 +0100

    [fix] start script

--- cam.sh
+++ cam.sh
@@ -4,25 +4,28 @@ cd /home/pi/autocam
 
 date
 
-if  [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]; then
+M=$(date +'%-M')
+H=$(date +'%-H')
+
+if   [ $(($M +  0)) == 0 ] && [ $(($H %  2)) == 0 ]; then
     printf 'Repo update..\r\n'
     git pull
     printf 'Repo update complete.\r\n'
 fi
 
-if   [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  4] == 0 ]; then
+if   [ $(($M +  0)) == 0 ] && [ $(($H %  4)) == 0 ]; then
     printf 'Autocam send and sync starting..\r\n'
     python3 cam.py --send --sync
     printf 'Autocam finished.\r\n'
-elif [ $[$(date +"%M") +  0] == 0 ] && [ $[$(date +"%H") %  2] == 0 ]; then
+elif [ $(($M +  0)) == 0 ] && [ $(($H %  2)) == 0 ]; then
     printf 'Autocam send starting..\r\n'
     python3 cam.py --send
     printf 'Autocam finished.\r\n'
-elif [ $[$(date +"%M") % 10] == 0 ] && [ $[$(date +"%H") %  1] == 0 ]; then
+elif [ $(($M % 10)) == 0 ] && [ $(($H %  1)) == 0 ]; then
     printf 'Autocam starting..\r\n'
     python3 cam.py
     printf 'Autocam finished.\r\n'
-elif [ $[$(date +"%M") %  5] == 0 ] && [ $[$(date +"%H") %  1] == 0 ]; then
+elif [ $(($M +  5)) == 0 ] && [ $(($H %  1)) == 0 ]; then
     printf 'Autocam for cloud only starting..\r\n'
     python3 cam.py --cloud
     printf 'Autocam finished.\r\n'
