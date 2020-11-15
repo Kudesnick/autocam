7440014f9505c208d853f3be3a988bd68c7446fa
Kudesnick <kudesnick@inbox.ru>
2020-11-15 21:50:17 +0000

    [mod] nocheck folder. It resolved problem of big filelist in folder

--- cam.py
+++ cam.py
@@ -129,7 +129,7 @@ def add_path_to_vk(_path: str, _send: bool):
 def add_to_cloud(_path: str):
     _cloud_path = 'cloud-mailru:/autocam_imgs' if not is_debug else 'cloud-mailru:/autocam_imgs_test'
     for _ in range(10):
-        if subprocess.run(['rclone', 'move', _path, _cloud_path, '--include', '*.jpg', '--max-transfer', '200M']) == 0:
+        if subprocess.run(['rclone', 'move', _path, _cloud_path, '--include', '*.jpg', '--max-transfer', '200M', '--no-check-dest']) == 0:
             return
 
 def main(_send_msg: bool = False, _cloud_only: bool = False, _cloud_sync: bool = False, _nopush: bool = False):
